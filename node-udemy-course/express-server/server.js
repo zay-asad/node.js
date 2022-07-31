3f9548a2077ecf040feb57a9012440a8db724e55
Zayan Asad <zayan.asad@sky.uk>
2022-07-31 12:55:11 +0100

    more changes

--- node-udemy-course/express-server/server.js
+++ node-udemy-course/express-server/server.js
@@ -1,16 +1,30 @@
 //creating an express server
 const express = require('express');
+// const bodyParser = require('body-parser');
+
 
 const app = express();
 
-//express has a concept of middleware
-app.use((req, res, next) => {
-    console.log('<h1>hellooooo</h1>')
-    next(); //express keep running through the next requests
-})
+
+// //express has a concept of middleware
+// app.use((req, res, next) => {
+//     console.log('<h1>hellooooo</h1>')
+//     next(); //express keep running through the next requests
+// })
+
+
+app.use(express.urlencoded({ extended: false })); //bodyparser
+app.use(express.json()); //bodyparser json (in case we're sending json from postman)
 
 
 app.get('/', (req, res) => {
+
+    //restful apis
+    // console.log(req.query); //http://localhost:3000/?name=bobi&age=18
+    // console.log(req.body)// req.body
+    // console.log(req.header); //use postman
+    // console.log(req.params);  //use postman
+
     // res.send("<h1>hellooo</h1");
     //sending json
     const user = {
@@ -21,16 +35,28 @@ app.get('/', (req, res) => {
     res.send(user);
 })
 
+//creating a 404 route
+app.get('/id', (req, res) => {
+    console.log(req.params)
+    res.status(404).send("page not found");
+})
+
 // additional route example
 app.get('/profile', (req, res) => {
     res.send('getting profile');
 })
 
 
-// post request - error since we are doing a get request on the browser
-// we are not sending any data (using post)
-app.post('/testing', (req, res) => {
-    res.send('testing route');
+// how to test:
+// 1. do a POST request using postman (node.js local app testing collection)
+// 2. check terminal (make sure the server js is running)
+app.post('/profile', (req, res) => {
+    console.log(req.body); //need to install body parser middleware to use this
+    const user = {
+        name: 'bob',
+        hobbies: 'gym'
+    }
+    res.send(user);
 })
 
 
