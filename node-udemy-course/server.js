5484b4c0eb77299bede5153f9561187f03ba4018
Zayan Asad <zayan.asad@sky.uk>
2022-07-31 11:00:23 +0100

    converting to a json response inside server.js

--- node-udemy-course/server.js
+++ node-udemy-course/server.js
@@ -7,8 +7,13 @@ const server = http.createServer((request, response) => {
     console.log('methods', request.method);
     console.log('url', request.url);
 
-    response.setHeader('Content-Type', 'text/html');
-    response.end('<h1>Hello</h1>');
+    const user = {
+        name: 'John',
+        hobby: 'coding'
+    }
+
+    response.setHeader('Content-Type', 'application/json');
+    response.end(JSON.stringify(user));
 })
 
 server.listen(3000);
